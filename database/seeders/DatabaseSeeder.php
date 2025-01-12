ca7594049abcac537862ca4b05bdda2693e5d4e9
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-01-12 12:02:26 +0800

    update seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Comment;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,9 +17,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
-        \App\Models\Category::factory(10)->create();
-        \App\Models\Post::factory(500)->create();
-        \App\Models\Comment::factory(1000)->create();
+        User::factory(10)->create();
+        Category::factory(10)->create();
+        Post::factory(500)->create();
+        Comment::factory(1000)->create();
+
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
+
+        User::each(function ($user) {
+            $user->assignRole('user');
+        });
+
+        $userAdmin = User::find(1)->assignRole('admin');
+        echo 'Admin : ' . $userAdmin->email . ' created' . PHP_EOL;
+        echo 'Password : password' . PHP_EOL;
+
+
     }
 }
